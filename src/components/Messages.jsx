9868b5113a49e9809aad446fe0349e0317b8bb3a
Ayush Singhal <ayush.singhal@thoughtworks.com>
2021-12-14 11:24:08 +0530

    add play and pause icon

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -2,11 +2,16 @@ import React, { useEffect, useRef, useState } from "react";
 import botIcon from "../images/bot-icon.svg";
 import userIcon from "../images/user-icon.svg";
 import { getTextToSpeech, getBotResponse } from "../api/chatService";
+import playIcon from "../images/play.svg";
+import pauseIcon from "../images/pause.svg";
 
 export default function Messages({ messages, latestMessage }) {
   const el = useRef(null);
   const audioEl = useRef();
   const [recordedAudio, setRecordedAudio] = useState("");
+  const [playAudio, setPlayAudio] = useState(false);
+  const [pauseAudio, setPauseAudio] = useState(false);
+
   const audio = audioEl.current;
   useEffect(() => {
     el.current.scrollIntoView({ block: "end", behavior: "smooth" });
@@ -23,15 +28,33 @@ export default function Messages({ messages, latestMessage }) {
           const audioURL = URL.createObjectURL(blob);
           setRecordedAudio(audioURL);
           audio?.play();
+          setPlayAudio(true);
+          setPauseAudio(false);
         }
       };
-      onPlayTextAudio();
+      // onPlayTextAudio();
     }
   }, [audio, latestMessage]);
 
-  const onStopAudio = async () => {
+  useEffect(() => {
+    audio?.addEventListener("ended", onEnded);
+    return () => {
+      audio?.removeEventListener("ended", onEnded);
+    };
+  });
+
+  const onEnded = () => {
+    setPlayAudio(false);
+    setPauseAudio(false);
+  };
+
+  const onPauseAudio = async () => {
     try {
-      if (audio) audio.pause();
+      if (audio) {
+        audio.pause();
+        setPauseAudio(true);
+        setPlayAudio(false);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -39,7 +62,11 @@ export default function Messages({ messages, latestMessage }) {
 
   const onReplayAudio = async () => {
     try {
-      if (audio) audio.play();
+      if (audio) {
+        audio.play();
+        setPauseAudio(false);
+        setPlayAudio(true);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -49,8 +76,18 @@ export default function Messages({ messages, latestMessage }) {
     <div className="chatArea">
       <span className="bot-icon">
         <img src={botIcon} alt="Chat Bot Icon" />
-        <button onClick={onStopAudio}>stop</button>
-        <button onClick={onReplayAudio}>play</button>
+        <div className="d-flex text-center">
+          {pauseAudio && (
+            <button onClick={onReplayAudio} className="play-icon">
+              <img src={playIcon} alt="Mic Icon" height="20" />
+            </button>
+          )}
+          {playAudio && (
+            <button onClick={onPauseAudio} className="play-icon">
+              <img src={pauseIcon} alt="Mic Icon" height="20" />
+            </button>
+          )}
+        </div>
       </span>
       <audio
         ref={audioEl}
