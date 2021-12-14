412a8deec1b1b3675cb899e248c44441ba9e4ee3
Ayush Singhal <ayush.singhal@thoughtworks.com>
2021-12-14 11:31:10 +0530

    change api port

--- server/app.ts
+++ server/app.ts
@@ -33,7 +33,7 @@ app.get("/hear", function (req, res) {
   gtts.stream().pipe(res);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
